950331bcc9dba40fa36a89bde4ec2fea339266af
JuancaJcA <avilafloresjc@gmail.com>
2021-05-03 00:28:59 -0400

    Assignment working :(

--- Juanca_Paper/js/assignment.js
+++ Juanca_Paper/js/assignment.js
@@ -1,30 +1,4 @@
-/************************************************************************
-*
-*  lap.js -- ported to javascript from
 
-   lap.cpp
-   version 1.0 - 4 September 1996
-   author: Roy Jonker @ MagicLogic Optimization Inc.
-   e-mail: roy_jonker@magiclogic.com
-
-   Code for Linear Assignment Problem, according to
-
-   "A Shortest Augmenting Path Algorithm for Dense and Sparse Linear
-    Assignment Problems," Computing 38, 325-340, 1987
-
-   by
-
-   R. Jonker and A. Volgenant, University of Amsterdam.
-
-*
-   PORTED TO JAVASCRIPT 2017-01-02 by Philippe Riviere(fil@rezo.net)
-   CHANGED 2016-05-13 by Yang Yong(yangyongeducation@163.com) in column reduction part according to
-   matlab version of LAPJV algorithm(Copyright (c) 2010, Yi Cao All rights reserved)--
-   https://www.mathworks.com/matlabcentral/fileexchange/26836-lapjv-jonker-volgenant-algorithm-for-linear-assignment-problem-v3-0:
-*
-*************************************************************************/
-
-/* This function is the jv shortest augmenting path algorithm to solve the assignment problem */
 function lap(dim, cost) {
   // input:
   // dim        - problem size
